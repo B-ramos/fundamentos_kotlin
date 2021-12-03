29b3748be91128761b65ab8b67648fcd1ca4e141
Bruno Ramos <bruno.ramos@performait.com>
2021-12-03 09:34:10 -0300

    lista

--- src/main/kotlin/fundamentos/list.kt
+++ src/main/kotlin/fundamentos/list.kt
@@ -1,13 +1,41 @@
-class Pessoa() {
+class Pessoa(var nome: String, var idade: Int ){
 
-    var nome: string;
-
-    Pessoa(){
-
+    override fun toString(): String {
+        return "Nome: ${nome}, Idade: ${idade}"
     }
 
 }
 
 fun main (){
+
+    var pessoas = mutableListOf<Pessoa>()
+
+    pessoas.add(Pessoa("Bruno", 36))
+    pessoas.add(Pessoa("Joao", 50))
+
+//    var idades = pessoas.map { it.idade }
+//    println(idades)
+//
+//
+//    var soma1 = pessoas.sumBy { it.idade }
+//    println(soma1)
+//
+//    var soma2 = pessoas.map { it.idade }.sum()
+//    println(soma2)
+//
+//    var nomes = pessoas.map { it.nome }
+//    println(nomes)
+
+//    var pessoa = pessoas.any{it.idade == 36}
+//    println(pessoa)// true
+//
+    var pessoa1 = pessoas.filter{ it.idade == 36}
+    println(pessoa1)// [Nome: Bruno, Idade: 36]
+
+    var pessoa2 = pessoas.singleOrNull{ it.idade == 36}
+    println(pessoa2)// Nome: Bruno, Idade: 36
+
+    var pessoa3 = pessoas.singleOrNull{ it.idade == 38}
+    println(pessoa3)// null
 
 }
